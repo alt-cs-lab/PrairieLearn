dca95d7360d12e071d859b04f23268c0b9c72350
Russell Feldhausen <russfeldh@gmail.com>
2023-06-12 13:59:35 -0500

    Update Tests & Fix Workflow

--- apps/prairielearn/src/tests/accessibility/index.test.js
+++ apps/prairielearn/src/tests/accessibility/index.test.js
@@ -214,6 +214,10 @@ const SKIP_ROUTES = [
   '/pl/loadFromDisk',
   '/pl/oauth2callback',
   '/pl/oauth2login',
+  // @russfeld start
+  '/pl/cascallback',
+  '/pl/caslogin',
+  // @russfeld end
 
   // Admin page; we aren't guaranteed to have subpages to navigate to.
   '/pl/administrator/batchedMigrations/:batched_migration_id',
